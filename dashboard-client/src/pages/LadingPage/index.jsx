c56d49cf8654c0782e9ef0d1471f08027d6fcdd9
ySanzinho <kenjirosan@outlook.com>
2022-03-11 16:09:18 -0300

    Atualização

--- dashboard-client/src/pages/LadingPage/index.jsx
+++ dashboard-client/src/pages/LadingPage/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Button } from '@chakra-ui/react';
 
 export function LadingPage( props ) {
+    const login = () => window.location.href = 'http://localhost:3001/api/auth/discord';
     return (
-        <h1>Lading Page</h1>
+        <>
+            <Button 
+            colorScheme='blue'
+            onClick={login}
+            >
+                Login Discord
+            </Button>
+        </>
     )
 }
